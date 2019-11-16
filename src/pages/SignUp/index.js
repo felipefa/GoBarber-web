7d15ed9ad64dca50211357b2890572ea78d8d899
Felipe Araujo <devaraujofelipe@gmail.com>
2019-11-16 18:07:06 -0300

    Add auth styles and unform

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="text" type="text" placeholder="Type your full name" />
+        <Input name="email" type="email" placeholder="Type your email" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Type your password"
+        />
+
+        <button type="submit">Create account</button>
+        <Link to="/">I already have an account</Link>
+      </Form>
+    </>
+  );
 }

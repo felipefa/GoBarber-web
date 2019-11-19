7fd87c78be4490fe88a6bcc8f221d7dc9db2b262
Felipe Araujo <devaraujofelipe@gmail.com>
2019-11-18 23:50:43 -0300

    Config app header

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: #b0c4de;
+  background: linear-gradient(-90deg, #7b68ee, ${darken(0.5, '#b0c4de')});
 `;

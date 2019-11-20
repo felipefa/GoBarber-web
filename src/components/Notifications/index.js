ae66eea7a5ebd584decce70d74af8189c1e31326
Felipe Araujo <devaraujofelipe@gmail.com>
2019-11-20 12:01:40 -0300

    Create notifications component

---
+++ src/components/Notifications/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import { MdNotifications } from 'react-icons/md';
+
+import {
+  Container,
+  Badge,
+  NotificationList,
+  Scroll,
+  Notification,
+} from './styles';
+
+export default function Notifications() {
+  return (
+    <Container>
+      <Badge hasUnread>
+        <MdNotifications color="#7b68ee" size={20} />
+      </Badge>
+
+      <NotificationList>
+        <Scroll>
+          <Notification unread>
+            <p>You have a new appointment for tomorrow</p>
+            <time>2 days ago</time>
+            <button type="button">Mark as read</button>
+          </Notification>
+          <Notification>
+            <p>You have a new appointment for tomorrow</p>
+            <time>2 days ago</time>
+            <button type="button">Mark as read</button>
+          </Notification>
+          <Notification>
+            <p>You have a new appointment for tomorrow</p>
+            <time>2 days ago</time>
+            <button type="button">Mark as read</button>
+          </Notification>
+          <Notification>
+            <p>You have a new appointment for tomorrow</p>
+            <time>2 days ago</time>
+            <button type="button">Mark as read</button>
+          </Notification>
+          <Notification>
+            <p>You have a new appointment for tomorrow</p>
+            <time>2 days ago</time>
+            <button type="button">Mark as read</button>
+          </Notification>
+          <Notification>
+            <p>You have a new appointment for tomorrow</p>
+            <time>2 days ago</time>
+            <button type="button">Mark as read</button>
+          </Notification>
+          <Notification>
+            <p>You have a new appointment for tomorrow</p>
+            <time>2 days ago</time>
+            <button type="button">Mark as read</button>
+          </Notification>
+        </Scroll>
+      </NotificationList>
+    </Container>
+  );
+}
